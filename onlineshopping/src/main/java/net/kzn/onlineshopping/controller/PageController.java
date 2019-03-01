1168be4626dcaa788ea979e5382c5e4b31b1682e
sonal barlekar <sonalbarlekar95@gmail.com>
2019-03-01 17:42:16 +0530

    completed loading static resource

---
+++ onlineshopping/src/main/java/net/kzn/onlineshopping/controller/PageController.java
@@ -0,0 +1,38 @@
+package net.kzn.onlineshopping.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+@Controller
+public class PageController {
+
+	@RequestMapping(value = {"/","/home","/index"})
+	public ModelAndView index() {
+		ModelAndView mv = new ModelAndView("page");
+		mv.addObject("Greeting","Welcome to web MVC");
+		return mv;
+	}
+	
+//	@RequestMapping(value="/test")
+//	public ModelAndView test(@RequestParam(value="Greeting" ,required=false)String Greeting) {
+//		if(Greeting==null) {
+//			Greeting="Hello There";
+//		}
+//		ModelAndView mv = new ModelAndView("page");
+//		mv.addObject("Greeting",Greeting);
+//		return mv;
+//	}
+	
+	
+//	@RequestMapping(value="/test/{Greeting}")
+//	public ModelAndView test(@PathVariable("Greeting")String Greeting) {
+//		if(Greeting==null) {
+//			Greeting="Hello There";
+//		}
+//		ModelAndView mv = new ModelAndView("page");
+//		mv.addObject("Greeting",Greeting);
+//		return mv;
+//	}
+}
