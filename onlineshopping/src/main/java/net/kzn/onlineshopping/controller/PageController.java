ebf396a634c893049a4cd7faee887294bce35920
sonal barlekar <sonalbarlekar95@gmail.com>
2019-03-04 18:50:20 +0530

    completed theme nd other issues

--- onlineshopping/src/main/java/net/kzn/onlineshopping/controller/PageController.java
+++ onlineshopping/src/main/java/net/kzn/onlineshopping/controller/PageController.java
@@ -1,5 +1,4 @@
 package net.kzn.onlineshopping.controller;
-
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,9 +10,29 @@ public class PageController {
 	@RequestMapping(value = {"/","/home","/index"})
 	public ModelAndView index() {
 		ModelAndView mv = new ModelAndView("page");
-		mv.addObject("Greeting","Welcome to web MVC");
+		mv.addObject("title","Home");
+		mv.addObject("userClickHome","true");
 		return mv;
 	}
+	
+	@RequestMapping(value = {"/about"})
+	public ModelAndView about() {
+		ModelAndView mv = new ModelAndView("page");
+		mv.addObject("title","About Us");
+		mv.addObject("userClickAbout","true");
+		return mv;
+	}
+	
+	@RequestMapping(value = {"/contact"})
+	public ModelAndView contact() {
+		ModelAndView mv = new ModelAndView("page");
+		mv.addObject("title","Contact Us");
+		mv.addObject("userClickContact","true");
+		return mv;
+	}
+	
+	
+	
 	
 //	@RequestMapping(value="/test")
 //	public ModelAndView test(@RequestParam(value="Greeting" ,required=false)String Greeting) {
---
+++ onlineshopping/src/main/webapp/WEB-INF/views/contact.jsp
@@ -0,0 +1,6 @@
+<div class="container">
+<div class="row">
+	<h1>This is my Contact us</h1>
+	<p>This is Contact us page.</p>
+</div>
+</div>
